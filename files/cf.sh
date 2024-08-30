9e0d7ea7548b2710e3da24acf89fcdfebe7b8623
welwel11 <dewiyu46@gmail.com>
2024-08-30 17:23:12 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -10,10 +10,10 @@ echo ""
 echo ""
 #sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
 read -rp "Input Domain Name. Example ( kontol12 ): " -e sub
-DOMAIN=serverpremium.biz.id
-SUB_DOMAIN=${sub}.serverpremium.biz.id
-CF_ID=vscobangst111@gmail.com
-CF_KEY=4a912a2d56a1b3837d48751da5334b74a4fb8
+DOMAIN=
+SUB_DOMAIN=${sub}.
+CF_ID=
+CF_KEY=
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
