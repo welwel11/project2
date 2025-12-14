8d2108939601e2ae72c7fab6a273e9d7df79058c
welwel11 <dewiyu46@gmail.com>
2025-12-15 04:39:19 +0700

    Add files via upload

--- setup2.sh
+++ setup2.sh
@@ -128,31 +128,50 @@ nginx_install() {
 
 # ==== BASE PACKAGE ====
 base_package() {
-    apt install zip pwgen openssl netcat socat cron bash-completion -y
-    apt install figlet -y
     apt update -y
     apt upgrade -y
     apt dist-upgrade -y
-    systemctl enable chronyd
-    systemctl restart chronyd
+
+    apt install -y \
+        zip pwgen openssl netcat socat cron bash-completion figlet \
+        sudo debconf-utils ntpdate \
+        software-properties-common \
+        speedtest-cli vnstat \
+        libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev \
+        libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison \
+        make libnss3-tools libevent-dev bc rsyslog dos2unix zlib1g-dev \
+        libssl-dev libsqlite3-dev sed dirmngr libxml-parser-perl \
+        build-essential gcc g++ python htop lsof tar wget curl ruby \
+        zip unzip p7zip-full python3-pip libc6 util-linux \
+        msmtp-mta ca-certificates bsd-mailx \
+        iptables iptables-persistent netfilter-persistent net-tools \
+        gnupg gnupg2 lsb-release shc cmake git screen socat xz-utils \
+        apt-transport-https gnupg1 dnsutils jq openvpn easy-rsa \
+        chrony
+
+    # === TIME SYNC (FIX UBUNTU 20.04) ===
+    systemctl stop systemd-timesyncd >/dev/null 2>&1
+    systemctl disable systemd-timesyncd >/dev/null 2>&1
+
     systemctl enable chrony
     systemctl restart chrony
-    chronyc sourcestats -v
-    chronyc tracking -v
-    apt install ntpdate -y
-    ntpdate pool.ntp.org
-    apt install sudo -y
-    sudo apt-get clean all
-    sudo apt-get autoremove -y
-    sudo apt-get install -y debconf-utils
-    sudo apt-get remove --purge exim4 -y
-    sudo apt-get remove --purge ufw firewalld -y
-    sudo apt-get install -y --no-install-recommends software-properties-common
+    chronyc tracking
+
+    # === NTP MANUAL SYNC (OPTIONAL) ===
+    ntpdate pool.ntp.org || true
+
+    # === CLEANUP ===
+    apt-get clean all
+    apt-get autoremove -y
+
+    # === REMOVE UNNEEDED SERVICES ===
+    apt-get remove --purge -y exim4 ufw firewalld
+
+    # === IPTABLES AUTO SAVE ===
     echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
     echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
-    sudo apt-get install -y speedtest-cli vnstat libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev bc rsyslog dos2unix zlib1g-dev libssl-dev libsqlite3-dev sed dirmngr libxml-parser-perl build-essential gcc g++ python htop lsof tar wget curl ruby zip unzip p7zip-full python3-pip libc6 util-linux build-essential msmtp-mta ca-certificates bsd-mailx iptables iptables-persistent netfilter-persistent net-tools openssl ca-certificates gnupg gnupg2 ca-certificates lsb-release gcc shc make cmake git screen socat xz-utils apt-transport-https gnupg1 dnsutils cron bash-completion ntpdate chrony jq openvpn easy-rsa
+
     print_success "Packet Yang Dibutuhkan"
-    
 }
 clear
 # ===== FUNGSI SETUP DOMAIN =====
@@ -727,6 +746,7 @@ enable_services(){
 
 # ===== INSTALL ALL =====
 instal(){
+set -e
     clear
 
     restart_system
