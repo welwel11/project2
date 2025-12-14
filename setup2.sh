1b6e06bd2fcb0b02d3fdf1a0f0875dd7951e4c73
welwel11 <dewiyu46@gmail.com>
2025-12-15 04:50:06 +0700

    Add files via upload

--- setup2.sh
+++ setup2.sh
@@ -461,24 +461,41 @@ print_success "Dropbear"
 # ===== VNSTAT =====
 ins_vnstat() {
     clear
-    print_install "Menginstall Vnstat"
-apt -y install vnstat > /dev/null 2>&1
-/etc/init.d/vnstat restart
-apt -y install libsqlite3-dev > /dev/null 2>&1
-wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
-tar zxvf vnstat-2.6.tar.gz
-cd vnstat-2.6
-./configure --prefix=/usr --sysconfdir=/etc && make && make install
-cd
-vnstat -u -i $NET
-sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
-chown vnstat:vnstat /var/lib/vnstat -R
-systemctl enable vnstat
-/etc/init.d/vnstat restart
-/etc/init.d/vnstat status
-rm -f /root/vnstat-2.6.tar.gz
-rm -rf /root/vnstat-2.6
-print_success "Vnstat"
+    print_install "Install vnStat 2.6"
+
+    systemctl stop vnstat 2>/dev/null
+    apt purge vnstat -y
+    rm -rf /var/lib/vnstat /etc/vnstat.conf
+
+    cd /root
+    wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
+    tar zxvf vnstat-2.6.tar.gz
+    cd vnstat-2.6
+    ./configure --prefix=/usr --sysconfdir=/etc
+    make
+    make install
+
+    NET=$(ip -o -4 route show to default | awk '{print $5}')
+    sed -i "s/^Interface.*/Interface \"$NET\"/" /etc/vnstat.conf
+
+    cat >/etc/systemd/system/vnstat.service <<'EOF'
+[Unit]
+Description=vnStat Network Traffic Monitor
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/vnstatd -n
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    systemctl enable vnstat
+    systemctl start vnstat
+
+    print_success "vnStat 2.6"
 }
 
 # ===== BACKUP SERVER =====
